aabfd4fc31187f24f774bf24fafb121f34b0972a
Xin Yin <xinyin@Xins-Air.lan>
2018-03-19 20:20:39 +1100

    Add DB script

--- wp-config.php
+++ wp-config.php
@@ -20,7 +20,7 @@
 
 // ** MySQL settings - You can get this info from your web host ** //
 /** The name of the database for WordPress */
-define('DB_NAME', 'i806193_wp8');
+define('DB_NAME', 'carrentalgeelong_database');
 
 /** MySQL database username */
 define('DB_USER', 'root');
